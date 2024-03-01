8560e2de8ef575af4fc2ea11187d835ce16aaf81
Jakub_Zieba <jakub.zieba@interia.pl>
2024-03-01 13:48:27 +0100

    Dodano pobieranie ksiazek

--- app/src/main/java/com/example/bookreader/BookDetailActivity.kt
+++ app/src/main/java/com/example/bookreader/BookDetailActivity.kt
@@ -1,19 +1,39 @@
 package com.example.bookreader
 
+import android.Manifest
+import android.app.ProgressDialog
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.example.bookreader.databinding.ActivityBookDetailBinding
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
+import java.io.FileOutputStream
+import kotlin.math.log
 
 class BookDetailActivity : AppCompatActivity() {
 
     private lateinit var binding:ActivityBookDetailBinding
 
+    private companion object{
+        const val TAG = "BOOK_DETAILS_TAG"
+    }
+
     private var bookId = ""
+
+    private var bookTitle = ""
+    private var bookUrl = ""
+
+    private lateinit var progressDialog: ProgressDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +41,10 @@ class BookDetailActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         bookId = intent.getStringExtra("bookId")!!
+
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Prosze czekać")
+        progressDialog.setCanceledOnTouchOutside(false)
 
         MyApplication.incrementBookViewCount(bookId)
 
@@ -31,9 +55,112 @@ class BookDetailActivity : AppCompatActivity() {
             intent.putExtra("bookId", bookId);
             startActivity(Intent(intent))
         }
+
+        binding.downloadBookBtn.setOnClickListener{
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                Log.d(TAG, "onCreate: Dostęp do schowka już jest przyznany")
+                downloadBook()
+            }
+            else{
+                Log.d(TAG, "onCreate: Dostęp do schowka odrzucony")
+                requestStoragePermissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            }
+        }
     }
 
+    private val requestStoragePermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){isGranted:Boolean ->
+        if (isGranted){
+            Log.d(TAG, "onCreate: Dostęp do schowka przyznany")
+            downloadBook()
+        }
+        else{
+            Log.d(TAG, "onCreate: Dostęp do schowka odrzucony")
+            Toast.makeText(this, "Dostęp do schowka odrzucony", Toast.LENGTH_SHORT).show()
+        }
+    }
 
+    private fun downloadBook(){
+
+        Log.d(TAG, "downloadBook: Pobieranie książki")
+        progressDialog.setMessage("Pobieranie książki")
+        progressDialog.show()
+
+        val storageReference = FirebaseStorage.getInstance().getReferenceFromUrl(bookUrl)
+        storageReference.getBytes(Constants.MAX_BYTES_PDF)
+            .addOnSuccessListener {bytes ->
+                Log.d(TAG, "downloadBook: Książka pobrana")
+                saveToDownloadsFolder(bytes)
+            }
+            .addOnFailureListener{e->
+                progressDialog.dismiss()
+                Log.d(TAG, "downloadBook: Nie udało się pobrać książki z powodu${e.message}")
+                Toast.makeText(this, "Nie udało się pobrać książki z powodu${e.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun saveToDownloadsFolder(bytes: ByteArray) {
+        Log.d(TAG, "saveToDownloadsFolder: zapisywanie pobranej książki")
+
+        val nameWithExtension = "$bookTitle.pdf"
+
+        try {
+            val downloadsFolder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+            downloadsFolder.mkdirs()
+
+            val filePath = downloadsFolder.path +"/"+ nameWithExtension
+
+            val out = FileOutputStream(filePath)
+            out.write(bytes)
+            out.close()
+
+            Toast.makeText(this, "Zapisano w folderze Pobrane", Toast.LENGTH_SHORT).show()
+            Log.d(TAG, "saveToDownloadsFolder: Zapisano w folderze Pobrane")
+            progressDialog.dismiss()
+            incrementDownloadCount()
+        }
+        catch (e: Exception){
+            progressDialog.dismiss()
+            Log.d(TAG, "saveToDownloadsFolder: nie udało się zapisać z powodu${e.message}")
+            Toast.makeText(this, "nie udało się zapisać z powodu${e.message}", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun incrementDownloadCount() {
+        Log.d(TAG, "incrementDownloadCount: ")
+
+        val ref = FirebaseDatabase.getInstance().getReference("Books")
+        ref.child(bookId)
+            .addListenerForSingleValueEvent(object: ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    var downloadsCount = "${snapshot.child("downloadCount").value}"
+                    Log.d(TAG, "onDataChange: Obecna ilość pobrań: $downloadsCount")
+
+                    if (downloadsCount == "" || downloadsCount == "null"){
+                        downloadsCount = "0"
+                    }
+
+                    val newDownloadCount: Long = downloadsCount.toLong() + 1
+                    Log.d(TAG, "onDataChange: Obecna ilość pobrań: $downloadsCount")
+
+                    val hashMap: HashMap<String, Any> = HashMap()
+                    hashMap["downloadCount"] = newDownloadCount
+
+                    val dbRef = FirebaseDatabase.getInstance().getReference("Books")
+                        dbRef.child(bookId)
+                            .updateChildren(hashMap)
+                            .addOnSuccessListener {
+                                Log.d(TAG, "onDataChange: Udało się zwiększyć wartość")
+                            }
+                            .addOnFailureListener{e->
+                                Log.d(TAG, "onDataChange: Nie udało się zwiększyć wartości z powodu ${e.message}")
+                            }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+            })
+    }
 
     private fun loadBookDetails() {
         val ref = FirebaseDatabase.getInstance().getReference("Books")
@@ -44,19 +171,19 @@ class BookDetailActivity : AppCompatActivity() {
                     val description = "${snapshot.child("description").value}"
                     val downloadCount = "${snapshot.child("downloadCount").value}"
                     val timestamp = "${snapshot.child("timestamp").value}"
-                    val title = "${snapshot.child("title").value}"
+                    bookTitle = "${snapshot.child("title").value}"
                     val uid = "${snapshot.child("uid").value}"
-                    val url = "${snapshot.child("url").value}"
+                    bookUrl = "${snapshot.child("url").value}"
                     val viewsCount = "${snapshot.child("viewsCount").value}"
 
                     val date = MyApplication.formatTimeStamp(timestamp.toLong())
 
                     MyApplication.loadCategory(categoryId, binding.categoryTv)
 
-                    MyApplication.loadBookFromUrlSinglePage("$url", "$title", binding.pdfView, binding.progressBar, binding.pagesTv)
-                    MyApplication.loadBookSize("$url", "$title", binding.sizeTv)
+                    MyApplication.loadBookFromUrlSinglePage("$bookUrl", "$bookTitle", binding.pdfView, binding.progressBar, binding.pagesTv)
+                    MyApplication.loadBookSize("$bookUrl", "$bookTitle", binding.sizeTv)
 
-                    binding.titleTv.text = title
+                    binding.titleTv.text = bookTitle
                     binding.descriptionTv.text = description
                     binding.viewsTv.text = viewsCount
                     binding.downloadsTv.text = downloadCount
